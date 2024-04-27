0e482602625ceb24b10a9317d5cba34d18892d39
JuanMontes <juaneduardo161214@gmail.com>
2024-04-26 19:20:03 -0500

    docs: updated document

--- src/app/medSystem/pages/treatments/treatments.component.ts
+++ src/app/medSystem/pages/treatments/treatments.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
+import {BaseService} from "../../../shared/services/base.service";
+
 
 @Component({
   selector: 'app-treatments',
   templateUrl: './treatments.component.html',
+  standalone: true,
   styleUrl: './treatments.component.css'
 })
 export class TreatmentsComponent {
+  constructor(private service:BaseService) {
 
+  }
 }
