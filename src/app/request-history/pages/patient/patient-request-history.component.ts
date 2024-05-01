be3187db3261c00b4664e0fcc74ef64f1655005f
CarlosG345 <u202210364@upc.edu.pe>
2024-05-01 13:03:02 -0500

    fix: fixed errors from patient-request-history typescript

--- src/app/request-history/pages/patient/patient-request-history.component.ts
+++ src/app/request-history/pages/patient/patient-request-history.component.ts
@@ -4,23 +4,25 @@ import {MatTableDataSource} from "@angular/material/table";
 import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
 import {PatientHistory} from "../../model/patient/patient-history";
+import {DoctorHistory} from "../../model/doctor/doctor-history.entity";
 
 
 @Component({
   selector: 'app-request-history',
   templateUrl: './doctor-request-history.component.html',
+  standalone: true,
   styleUrl: './doctor-request-history.component.css'
 })
-export class DoctorRequestHistoryComponent {
-  historyList !: PatientHistory[];
+export class PatientRequestHistoryComponent {
+  historyList !: DoctorHistory[];
   dataSource :any;
   displayedColumns = ['patientName','campus', 'speciality', 'doctorName', 'dateTime'];
   @ViewChild(MatPaginator) paginator !: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  constructor(private service:BaseService<PatientHistory>) {
+  constructor(private service:BaseService<DoctorHistory>) {
     this.service.getAppointmentsWithHistory().subscribe(res => {
       this.historyList = res.flatMap(appointment => appointment.requestHistory);
-      this.dataSource = new MatTableDataSource<PatientHistory>(this.historyList);
+      this.dataSource = new MatTableDataSource<DoctorHistory>(this.historyList);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     })
