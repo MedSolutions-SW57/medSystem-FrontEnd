f1f0a713a093199037a4fddd7d4f846ff5bd7a5b
JuanMontes18 <u202210775@upc.edu.pe>
2024-06-09 22:37:49 -0500

    feat: update bug

--- src/app/treatments/pages/pacient/pacient-treatments.component.ts
+++ src/app/treatments/pages/pacient/pacient-treatments.component.ts
@@ -1,4 +1,5 @@
 import {Component, ViewChild} from '@angular/core';
+// @ts-ignore
 import {Treatment} from "../../../medSystem/model/treatment.entity";
 import {BaseService} from "../../../shared/services/base.service";
 
@@ -11,7 +12,7 @@ export class PacientTreatmentsComponent {
   treatments !:Treatment[];
   dataSource: any;
   displayedColumns=["date","treatment-type","doctor","status"]
-  constructor(private service:BaseService) {
+  constructor(private service:BaseService<any>) {
     this.service.getTreatments().subscribe(res => {
       this.treatments = res;
     });
