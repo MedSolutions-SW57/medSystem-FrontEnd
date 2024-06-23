275e4043dd39c0e21de684ff2f3996b5a4338d5e
jorgealtamirano1 <u202215888@upc.edu.pe>
2024-06-23 18:28:16 -0500

    feat: added doctor and patient entities

---
+++ src/app/profiles/model/patient.entity.ts
@@ -0,0 +1,12 @@
+export class Patient {
+  constructor(public firstName : string,
+              public lastName : string,
+              public email : string,
+              public phone : string,
+              public street : string,
+              public number: string,
+              public city : string,
+              public postalCode : string,
+              public country : string,
+              public userId: number) {}
+}
