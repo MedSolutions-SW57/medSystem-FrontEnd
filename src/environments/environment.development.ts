45df94bcc124b73b400be1cb2dca49d862e91fed
u202215888 <u202215888@upc.edu.pe>
2024-06-26 17:11:40 -0500

    feat: added creation of new reports

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   //serverBasePath: 'http://localhost:8080/api/v1'
-  serverBasePath: 'https://medsystem-backend-production.up.railway.app/api/v1'
+  serverBasePath: 'medsystem-backend-production-3b64.up.railway.app/api/v1'
 };
