1fc952545da47a7ab3fef10ed23e2d34ca311986
jorgealtamirano1 <u202215888@upc.edu.pe>
2024-06-26 13:29:24 -0500

    chore: final modification of environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  serverBasePath: 'http://localhost:8080/api/v1'
-  //serverBasePath: 'https://med-system-production.up.railway.app/api/v1'
+  //serverBasePath: 'http://localhost:8080/api/v1'
+  serverBasePath: 'https://medsystem-backend-production.up.railway.app/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  serverBasePath: 'http://localhost:8080/api/v1'
-  //serverBasePath: 'https://med-system-production.up.railway.app/api/v1'
+  //serverBasePath: 'http://localhost:8080/api/v1'
+  serverBasePath: 'https://medsystem-backend-production.up.railway.app/api/v1'
 };
